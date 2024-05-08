7709100fc9b958022011d02bfcac92183ed78f72
NATACHAOS <164019299+NATACHAOS@users.noreply.github.com>
2024-05-08 14:42:41 +0300

    Add files via upload

--- special_bound.py
+++ special_bound.py
@@ -2,13 +2,13 @@ class House:
     """ДОМ"""
 
     def __init__(self):
-        self.numberOfFloors = 0 # задаём атрибут этажности
+        self.numberOfFloors = 0  # задаём атрибут этажности
 
-    def setNewNumberOfFloors(self):  # изменим атрибут этажности на floors и выведем на консоль
-        self.floors = floors = 10
+    def setNewNumberOfFloors(self, floors):  # изменим атрибут этажности на floors и выведем на консоль
         self.numberOfFloors = floors
         print(self.numberOfFloors)
 
 
 my_house = House()
-my_house.setNewNumberOfFloors()
+my_house.setNewNumberOfFloors(floors="10")
+
